0a197d2502a0226d8b4f37fa3fc467c1439fd22b
Mario Picasso <mariopicassodev@gmail.com>
2024-08-11 19:59:11 -0300

    OK: en and es languages

---
+++ src/app/[lang]/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { i18n } from "../../../i18n-config";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export async function generateStaticParams() {
+    return i18n.locales.map((locale) => ({ lang: locale }));
+}
+
+export const metadata = {
+    title: "Events App",
+    description: "Events app with two languages"
+};
+
+export default function RootLayout({ children, params }) {
+    return (
+        <html lang={params.lang}>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
