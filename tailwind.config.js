7fd025fa353548636315c54f0ea084eaf24442d2
Mario Picasso <mariopicassodev@gmail.com>
2024-08-11 20:14:30 -0300

    OK: Added daysiUI plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
