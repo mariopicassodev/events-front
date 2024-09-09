bf9aea0732df854c97b85c02e2e7ab01ae4a2af0
Mario Picasso <mariopicassodev@gmail.com>
2024-09-09 19:36:02 -0300

    FT: show list of reservations

--- src/app/[lang]/my-events/page.js
+++ src/app/[lang]/my-events/page.js
@@ -11,7 +11,7 @@ export default async function myEvents({params: {lang}}) {
             <h1 className="text-2xl font-bold mb-4 text-center">{dictionary.myEvents.title}</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4">
                 {myEvents.data.data.userEvents.map((event) => (
-                    <MyEventCard key={event.id} event={event} dictionary={dictionary} />
+                    <MyEventCard key={event.id} event={event} dictionary={dictionary} lang={lang}/>
                 ))}
             </div>
         </div>
